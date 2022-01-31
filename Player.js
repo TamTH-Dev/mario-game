9ae2d795bf4af42a8e01b39d4ef685f012b194b4
TamTH-Dev <tamth.dev@gmail.com>
2022-01-31 15:43:10 +0700

    Add player

--- Player.js
+++ Player.js
@@ -1,6 +1,17 @@
 class Player {
-  constructor() {
-    console.log('Player is created')
+  constructor(ctx) {
+    this.position = {
+      x: 100,
+      y: 100,
+    }
+    this.width = 30
+    this.height = 30
+    this.ctx = ctx
+  }
+
+  draw() {
+    this.ctx.fillStyle = '#ff2200'
+    this.ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
 }
 
--- index.js
+++ index.js
@@ -3,5 +3,20 @@ import Player from './Player.js'
 const canvas = document.querySelector('#canvas')
 const ctx = canvas.getContext('2d')
 
-const player = new Player()
-console.log(player, ctx)
+window.addEventListener('load', init)
+window.addEventListener('resize', setupViewport)
+
+function init() {
+  setupViewport()
+  setupPlayer()
+}
+
+function setupViewport() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+}
+
+function setupPlayer() {
+  const player = new Player(ctx)
+  player.draw()
+}
